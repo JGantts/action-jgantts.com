3dff2a062061d3f35e07184da985c5757cb26f7b
Jacob Gantt <gantt42@gmail.com>
2022-06-24 22:40:09 -0400

    Add fs dependency

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const fs = require('fs');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    try {
+        core.info(`JGantts.com Custom Deployment`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        //const files = fs.readdirSync('');
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        // files object contains all files names
+        // log them on console
+        /*files.forEach(file => {
+            core.info(`${file}`);
+        });*/
+
+        const ms = core.getInput('milliseconds');
+        core.info(`Waiting ${ms} milliseconds ...`);
+
+        core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        await wait(parseInt(ms));
+        core.info((new Date()).toTimeString());
+
+        core.setOutput('time', new Date().toTimeString());
+    } catch (error) {
+        core.info(`Wut?`);
+        core.setFailed(error.message);
+    }
 }
 
 run();
