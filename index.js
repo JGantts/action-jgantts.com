df116efa00d3fe4cf349156832142e35c9ba1037
Jacob Gantt <gantt42@gmail.com>
2022-06-24 23:36:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const wait = require('./wait');
 const fs = require('fs');
-
+const exec = require('child_process').execFile;
 
 // most @actions toolkit packages have async methods
 async function run() {
